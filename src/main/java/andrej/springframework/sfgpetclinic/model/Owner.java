2423a7bef2c613c74095db025da7e91c5e1ccf08
Andrej Georgiev <emperorandrej@yahoo.com>
2019-01-23 05:06:26 +0100

    added POJOS

---
+++ src/main/java/andrej/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package andrej.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+
+}
---
+++ src/main/java/andrej/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package andrej.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
