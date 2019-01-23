2c33b80b1a5e6de739b79214c03b44028c88620d
Andrej Georgiev <emperorandrej@yahoo.com>
2019-01-23 07:45:26 +0100

    Added abstract service implementation

---
+++ src/main/java/andrej/springframework/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,37 @@
+package andrej.springframework.sfgpetclinic.service.map;
+
+import andrej.springframework.sfgpetclinic.model.Vet;
+import andrej.springframework.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+/**
+ * Created by jt on 7/21/18.
+ */
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
